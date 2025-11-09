909163d8df26ca3a791b7997c0532740fbb554d5
Clarissa Adela Garcia <124195524+Adelacg25@users.noreply.github.com>
2025-11-09 10:45:35 -0600

    summary done

--- src/renderer/src/components/ServicePage.jsx
+++ src/renderer/src/components/ServicePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import ResSumm from "./SerSumm";
 
 const buttonStyle = {
   padding: "8px 12px",
@@ -161,7 +162,9 @@ export default function ServicePage() {
               onClick={() => handleDeleteClick(service)}
             > DELETE </button>
 
+            <Link to={`/service-summary/${service.service_id}`} style={{ textDecoration: "none" }}>
             <button style={buttonStyle}>SUMMARY</button>
+            </Link>
           </div>
         );
       })}
